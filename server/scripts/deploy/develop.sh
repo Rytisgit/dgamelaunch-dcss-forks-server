70ff31a070af4f108466a11e34dcbb6de5d614b4
Rytis Petronis <petronisrytis@gmail.com>
2025-05-21 01:42:43 +0200

    update

--- server/scripts/deploy/develop.sh
+++ server/scripts/deploy/develop.sh
@@ -7,5 +7,5 @@ mv docker-compose.combine.yml docker-compose.yml && \
 rm docker-compose.ports.yml && \
 docker compose down && docker rmi refracta/dcss-server || true && \
 docker compose run --rm -T -e CMD='cd $DGL_CONF_HOME && git pull' dcss-server && \
-docker compose run --rm -T -e CMD='$SCRIPTS/utils/release.sh download -o -p /data -n game-data' dcss-server && \
+docker compose run --rm -T -e CMD='$SCRIPTS/game/install-crawl-versions.sh' dcss-server && \
 docker compose up -d && docker compose logs -f
