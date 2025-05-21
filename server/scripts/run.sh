a6345b09a18740902035de02bd3575d76380e393
Rytis Petronis <petronisrytis@gmail.com>
2025-05-22 01:38:55 +0200

    cgh

--- server/scripts/run.sh
+++ server/scripts/run.sh
@@ -8,7 +8,7 @@ service nginx start
 
 "$SCRIPTS"/utils/setup-ssh-keys.sh
 /etc/init.d/ssh start
-sudo -u crawl nohup ttyd -p 8022 -t 'theme={"background": "#000000"}' -W dgamelaunch &
+#sudo -u crawl nohup ttyd -p 8022 -t 'theme={"background": "#000000"}' -W dgamelaunch &
 
 # Limits CPU usage for builds performed with cron.
 nohup cpulimit -e cc1plus -l 20 &
@@ -17,5 +17,5 @@ nohup cpulimit -e bzip2 -l 20 &
 
 dgl crawl-inotify-dglwhere
 
-rm "$CHROOT_WEBDIR/run/webtiles.pid" #in case the container was stopped without cleaning up pid file in volume
-/etc/init.d/webtiles restart
+#rm "$CHROOT_WEBDIR/run/webtiles.pid" #in case the container was stopped without cleaning up pid file in volume
+/etc/init.d/webtiles start
