e4c65282c1a458481e57e2be65a75dd82e9180e5
Rytis Petronis <petronisrytis@gmail.com>
2025-05-22 01:49:50 +0200

    remove other versions

--- server/scripts/game/install-crawl-versions.sh
+++ server/scripts/game/install-crawl-versions.sh
@@ -10,18 +10,4 @@ for v in $VERSIONS; do
 done
 
 dgl update-trunk
-#for version in {33..11}; do
-#  dgl update-stable 0.$version
-#done
-#dgl update-gcc dcssca crawl-forks/dcssca/bugfix 6
-#dgl update-gcc hellcrawl crawl-forks/hellcrawl/bugfix 6
-#dgl update-gcc gnollcrawl crawl-forks/gnollcrawl/bugfix 6
-#dgl update-gcc bcrawl bcrawl/master
-#dgl update-gcc bloatcrawl2 bloatcrawl2/master 7
-#dgl update-gcc stoatsoup stoatsoup/master
-#dgl update-gcc gooncrawl gooncrawl/gooncrawl_stable 7
-#dgl update-gcc xcrawl crawl-forks/xcrawl/bugfix 7
-#dgl update-gcc bcadrencrawl bcadrencrawl/bCrawl
-#dgl update-gcc kimchicrawl crawl-forks/kimchicrawl/bugfix 7
-#dgl update-gcc addedcrawl crawl-forks/addedcrawl/bugfix 7
 dgl update-gcc dcst dcst/test
