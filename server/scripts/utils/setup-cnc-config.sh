a76e76eb0c38ba75eae489922adf0c36b4397dee
Rytis Petronis <petronisrytis@gmail.com>
2025-05-22 10:51:11 +0200

    don't check for non installed games, which freezes the startup

--- server/scripts/utils/setup-cnc-config.sh
+++ server/scripts/utils/setup-cnc-config.sh
@@ -1,17 +1,6 @@
 #!/bin/bash
 
 source "$DGL_CONF_HOME/dgl-manage.conf"
-sed -i 's|CONFIG_MORGUE_URL|https://archive.nemelex.cards/morgue/%n/|g' "$DGL_CONF_HOME/config.py"
-sed -i 's|CONFIG_SERVER_ID|crawl.nemelex.cards|g' "$DGL_CONF_HOME/config.py"
-sed -i 's|CONFIG_DGL_SERVER|crawl.nemelex.cards|g' "$DGL_CONF_HOME/dgl-manage.conf"
-sed -i 's|CONFIG_WEB_SAVEDUMP_URL|https://archive.nemelex.cards/saves|g' "$DGL_CONF_HOME/dgl-manage.conf"
-cp -r $DGL_CONF_HOME/server/etc/webserver/* $WEBDIR
-# TODO: delete localStorage.removeItem("DWEM"); should be removed (temporal setting)
-sed -i 's|<script type="text/javascript">|<script  type="text/javascript">\
-      localStorage.removeItem("DWEM");\
-      localStorage.DWEM_MODULES = JSON.stringify(\
-        ["io-hook", "site-information", "websocket-factory", "rc-manager", "module-manager", "cnc-banner", "cnc-userinfo", "sound-support", "cnc-chat", "cnc-public-chat", "convenience-module", "cnc-splash-screen", "wtrec", "advanced-rc-editor", "translation-module"].map(m => "../modules/" + m + "/index.js")\
-      );|g' "$WEBDIR/templates/client.html"
-grep -qxF '# CRAWL.NEMELEX.CARDS' /dgldir/data/crawl-git-settings/init.txt || sed -i '1i# CRAWL.NEMELEX.CARDS' /dgldir/data/crawl-git-settings/init.txt
+sed -i 's|games = OrderedDict(trunk + stable_versions + forks)|games = OrderedDict(trunk + forks)' "$DGL_CONF_HOME/config.py"
 dgl publish --confirm > /dev/null 2>&1
 echo
